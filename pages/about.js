51715483216e295066eb80d67f9704704924c418
martin-bass <martin_reym@hotmail.com>
2022-06-13 20:08:48 -0300

    cambios en about y detalle del perfil

--- pages/about.js
+++ pages/about.js
@@ -1,12 +1,25 @@
 import React from 'react';
 import Container from '../container/container';
+import Link from "next/link";
+
 
 function About() {
   return (
     <Container>
-      <h1>About</h1>
+      <div style={{display: 'flex', alignItems: 'center', justifyContent: 'center'}}>
+        <div className="card bg-light mb-3 " style={{width: '60%', margin: '20px'}}>
+          <div className="card-header">About</div>
+          <div className="card-body">
+            <h4 className="card-title">About</h4>
+            <p className="card-text">Ejemplo de buscador de perfiles, desarrollado con React.JS, Next.JS y con llamado a API externa.</p>
+          </div>
+          <div className='m-2'style={{display: 'flex', alignItems: 'center', justifyContent: 'center'}}>
+            <Link href='/'><button type="button" className="btn btn-info">Back</button></Link>
+          </div>
+        </div>
+      </div>
     </Container>
   )
-}
+};
 
-export default About
+export default About;
--- pages/users/[id].js
+++ pages/users/[id].js
@@ -2,6 +2,13 @@ import React from 'react';
 import {useRouter} from 'next/router';
 import fetch from 'isomorphic-fetch';
 import Container from '../../container/container';
+import styles from './[id].module.css';
+import Link from "next/link";
+
+
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootswatch/dist/flatly/bootstrap.min.css';
 
 function UsefProfile({user}) {
 
@@ -12,7 +19,23 @@ function UsefProfile({user}) {
 
   return (
     <Container>
-      <div>UsefProfile {user.id}</div>
+      <div className={styles.CardDetailContainer}>
+        <div className="card w-25 m-5" key={user.id}>
+          <div className={styles.titleContainer}>
+              <h3 className="card-header h-100">{user.first_name} {user.last_name}</h3>
+          </div>
+          <div className="card-body">
+              <h4 className="card-title">E-mail:</h4>
+              <h6 className="card-subtitle text-muted">{user.email}</h6>
+          </div>
+          <div className={styles.imgContainer}>
+              <img className={styles.userImg} src={user.avatar}></img>
+          </div>
+          <div className="card-footer text-muted">
+            <Link href='/'><button type="button" className="btn btn-info">Back</button></Link>
+          </div>
+        </div>
+      </div>   
     </Container>
   )
 };
